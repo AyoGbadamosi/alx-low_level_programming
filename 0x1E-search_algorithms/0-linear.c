29659c701249992892a987a5fb3f97743551d4c5
AyoGbadamosi <gbadamosiayo16@gmail.com>
2022-10-04 05:42:02 -0700

    changes to linear search

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -11,16 +11,14 @@
 int linear_search(int *array, size_t size, int value)
 {
 	size_t i;
-	int index;
 
+	if (!array)
+		return (-1);
 	for (i = 0; i < size; i++)
 	{
 		printf("Value checked array[%ld] = [%d]\n", i, array[i]);
 		if (array[i] == value)
-		{
-			index = i;
-			return (index);
-		}
+			return (i);
 	}
 	return (-1);
 }
